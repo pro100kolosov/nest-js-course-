96164de042c6f158786ff9dc4aad2d5d151e2375
Dmitry <kolosovdebora@gmail.com>
2021-10-06 15:34:18 +0300

    feat: create mdels

---
+++ src/review/review.model.ts
@@ -0,0 +1,6 @@
+export class ReviewModel {
+  name: string;
+  title: string;
+  descripion: string;
+  ceatedAt: Date;
+}
---
+++ src/top-page/top-page.model.ts
@@ -0,0 +1,25 @@
+export enum TopLevelCategry {
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+export class TopPageModel {
+  firstCategory: TopLevelCategry;
+  secondCategory: string;
+  title: string;
+  category: string;
+  hh?: {
+    count: number;
+    juniorSalary: number;
+    middleSalary: number;
+    seniorSalary: number;
+  };
+  advantages: {
+    title: string;
+    description: string;
+  }[];
+  seoText: string;
+  tagsTitle: string;
+  tags: string[];
+}
