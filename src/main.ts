6dae8181d910d849e7db857c849a76b076d442e5
Dmitry <kolosovdebora@gmail.com>
2021-10-08 15:00:39 +0300

    feat: create controller auth

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // теперь все запросы к API должны иметь в своём URL сначала /api
   await app.listen(3000);
 }
 bootstrap();
