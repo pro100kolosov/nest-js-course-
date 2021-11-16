b16c9621f02eec680862c698ef0c1060f123948d
Dmitry <kolosovdebora@gmail.com>
2021-11-16 15:48:19 +0300

    init

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,9 +1,20 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FindTopPageDTO } from './dto/find-top-page.dto';
 import { TopPageModule } from './top-page.module';
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly configService: ConfigService) {}
   @Post('create')
   async create(@Body() dto: Omit<TopPageModule, '_id'>) {}
   @Get(':id')
